b724bfb68fa281e0f381f0f8f38b8f08afd3046f
pineappleok <550185271@qq.com>
2018-05-09 13:08:34 +0800

    1、基本完成头部布局；

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,121 @@
 <style lang="less">
     @import "./main.less";
+.ivu-menu-light{
+    height: 64px;
+    background:linear-gradient(to bottom, #2945CB, #2C83F9);
+}
+.layout-logo{
+    width: 304px;
+    height: 66px;
+    float: left;
+    position: relative;
+    top: 10px;
+    left: 16px;
+    img{
+        width:155px;
+        height: 40px;
+    }
+}
+.layout-nav{
+    text-align: center;
+}
+.ivu-menu-horizontal{
+    .ivu-menu-item,.ivu-menu-submenu{
+        float:none;
+        display:inline-block;
+    } 
+}
+.ivu-menu-light.ivu-menu-horizontal{
+    .ivu-menu-item,.ivu-menu-submenu{
+        color:rgba(255,255,255,0.8);
+    }
+    .ivu-menu-item-active,.ivu-menu-item:hover,.ivu-menu-submenu-active,.ivu-menu-submenu:hover{
+    color:#fff;
+    }
+}
+.top-header .header-avator-con .message-con {
+    display: inline-block;
+    width: 30px;
+    margin-right: 90px;
+    text-align: center;
+    cursor: pointer;
+}
+.top-header .header-avator-con .message-con i {
+  vertical-align: middle;
+}
+.top-header .header-avator-con .user-dropdown-menu-con {
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 150px;
+  height: 100%;
+}
+.top-header .header-avator-con .user-dropdown-menu-con .main-user-name {
+  display: inline-block;
+  width: 80px;
+  word-break: keep-all;
+  white-space: nowrap;
+  vertical-align: middle;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  text-align: right;
+}
+.top-header .search{
+    display: inline-block;
+}
+.top-header .ivu-btn-primary{
+    background:transparent;
+    border:none;
+}
 </style>
 <template>
     <div class="main" :class="{'main-hide-text': shrink}">
-        <div class="sidebar-menu-con" :style="{width: shrink?'60px':'200px', overflow: shrink ? 'visible' : 'auto'}">
+        <div class="top-header">   
+            <Menu mode="horizontal" active-name="1">
+                <Row>
+                    <Col span="4">
+                        <div class="layout-logo"><img src="../images/logo.png" alt=""></div>
+                    </Col>
+                    <Col span="16" style="text-align:center;">
+                        <div class="layout-nav">
+                            <MenuItem name="1">
+                                产品创建
+                            </MenuItem>
+                            <MenuItem name="2">
+                                运维管理
+                            </MenuItem>
+                            <MenuItem name="3">
+                                运营分析
+                            </MenuItem>
+                            <MenuItem name="4">
+                                开发文档
+                            </MenuItem>
+                        </div>
+                    </Col>
+                    <Col span="4" style="text-align:right;">
+                        <div class="header-avator-con">
+                            <div class="search"><Icon type="search" :size="20" style="margin-right:10px;color:#fff;vertical-align:middle;"></Icon></Button></div>
+                            <message-tip v-model="mesCount"></message-tip>
+                            <div class="user-dropdown-menu-con">
+                                <Row type="flex" justify="end" align="middle" class="user-dropdown-innercon">
+                                    <Avatar :src="avatorPath" style="background: #619fe7;margin-right: 10px;"></Avatar>
+                                    <Dropdown transfer trigger="click" @on-click="handleClickUserDropdown">
+                                        <a href="javascript:void(0)">
+                                            <Icon type="chevron-down" :size="14"></Icon>
+                                        </a>
+                                        <DropdownMenu slot="list">
+                                            <DropdownItem name="ownSpace">个人中心</DropdownItem>
+                                            <DropdownItem name="loginout" divided>退出登录</DropdownItem>
+                                        </DropdownMenu>
+                                    </Dropdown>
+                                </Row>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+            </Menu>
+        </div>
+        <div v-if="sidebarPage" class="sidebar-menu-con" :style="{width: shrink?'60px':'200px', overflow: shrink ? 'visible' : 'auto'}">
             <scroll-bar ref="scrollBar">
                 <shrinkable-menu 
                     :shrink="shrink"
@@ -19,9 +131,9 @@
                 </shrinkable-menu>
             </scroll-bar>
         </div>
-        <div class="main-header-con" :style="{paddingLeft: shrink?'60px':'200px'}">
+        <div class="main-header-con" :style="{paddingLeft: !sidebarPage?'0':(shrink?'60px':'200px')}">
             <div class="main-header">
-                <div class="navicon-con">
+                <div class="navicon-con" v-if="sidebarPage">
                     <Button :style="{transform: 'rotateZ(' + (this.shrink ? '-90' : '0') + 'deg)'}" type="text" @click="toggleClick">
                         <Icon type="navicon" size="32"></Icon>
                     </Button>
@@ -29,28 +141,6 @@
                 <div class="header-middle-con">
                     <div class="main-breadcrumb">
                         <breadcrumb-nav :currentPath="currentPath"></breadcrumb-nav>
-                    </div>
-                </div>
-                <div class="header-avator-con">
-                    <full-screen v-model="isFullScreen" @on-change="fullscreenChange"></full-screen>
-                    <lock-screen></lock-screen>
-                    <message-tip v-model="mesCount"></message-tip>
-                    <theme-switch></theme-switch>
-                    
-                    <div class="user-dropdown-menu-con">
-                        <Row type="flex" justify="end" align="middle" class="user-dropdown-innercon">
-                            <Dropdown transfer trigger="click" @on-click="handleClickUserDropdown">
-                                <a href="javascript:void(0)">
-                                    <span class="main-user-name">{{ userName }}</span>
-                                    <Icon type="arrow-down-b"></Icon>
-                                </a>
-                                <DropdownMenu slot="list">
-                                    <DropdownItem name="ownSpace">个人中心</DropdownItem>
-                                    <DropdownItem name="loginout" divided>退出登录</DropdownItem>
-                                </DropdownMenu>
-                            </Dropdown>
-                            <Avatar :src="avatorPath" style="background: #619fe7;margin-left: 10px;"></Avatar>
-                        </Row>
                     </div>
                 </div>
             </div>
@@ -92,7 +182,8 @@
         },
         data () {
             return {
-                shrink: false,
+                sidebarPage:false,
+                shrink: true,
                 userName: '',
                 isFullScreen: false,
                 openedSubmenuArr: this.$store.state.app.openedSubmenuArr

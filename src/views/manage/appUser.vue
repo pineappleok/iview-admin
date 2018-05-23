7caa6183bb0a31d0093b2c022afe1a040db3ba99
pineappleok <550185271@qq.com>
2018-05-23 19:30:01 +0800

    1、原生checkbox也没有全选上；

---
+++ src/views/manage/appUser.vue
@@ -0,0 +1,216 @@
+<!-- 运维管理-产品库管理 -->
+<template>
+    <div class="manage-right appUser">
+        <div class="top-bar clearfix">
+            APP用户管理
+            <div class="fr">
+                <Select v-model="productModel" style="width:160px" placeholder="产品类型">
+                    <Option v-for="item in productModelList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                </Select>
+                <Input class="search" v-model="searchId" icon="ios-search" placeholder="用户帐号..." style="width: 160px"></Input>
+                <Select v-model="productCondition" style="width:160px" placeholder="产品版本">
+                    <Option v-for="item in productConditionList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+                </Select>
+            </div>
+        </div>
+        <div class="manage-content">
+            <div class="content">
+                <h3>用户列表
+                    <Button type="ghost" class="ghost-blue fr">密码复位</Button>
+                </h3>
+                <table class="mytable">
+                    <thead>
+                        <tr>
+                            <tr>
+                                <th width="5%">
+                                    <!-- <Checkbox v-model="tableData.allChecked" @on-change="checkAll(tableData)"></Checkbox> -->
+                                    <input type="checkbox" v-model="tableData.allChecked" @change="checkAll(tableData)" />
+                                </th>
+                                <th width="12.5%">序号</th>
+                                <th width="12.5%">用户账号</th>
+                                <th width="17%">地区</th>
+                                <th width="12.5%">手机品牌</th>
+                                <th width="12.5%">APP版本</th>
+                                <th width="20%">注册时间</th>
+                                <th width="8%">绑定产品数量</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(item,index) in tableData.rows" :key="index">
+                            <td>
+                                <!-- <Checkbox v-model="item.checked" @on-change="check(tableData,item,index)"></Checkbox> -->
+                                <input type="checkbox" v-model="item.checked" @change="check(tableData,item,index)" />
+                            </td>
+                            <td>{{item.no}}</td>
+                            <td>{{item.account}}</td>
+                            <td>{{item.city}}</td>
+                            <td>{{item.mobile}}</td>
+                            <td>{{item.version}}</td>
+                            <td>{{item.regisetrTime}}</td>
+                            <td>{{item.num}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <Page :total="100" :page-size="10" :current="1" @on-change="changePage" class="page"></Page>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import echarts from 'echarts'
+    export default {
+        name: "appUser",
+        data() {
+            return {
+                tableData: {},
+                productModel: "",
+                productModelList: {},
+                searchId: "",
+                productCondition: "",
+                productConditionList: {}
+            };
+        },
+        beforeCreate() {
+            // 传值给父级main.vue（第二个导航）
+            this.$emit("set-manage-menu", "appUser");
+        },
+        mounted() {
+            this.mockTableData();
+            this.mockproductModelListData();
+            this.mockproductConditionListData();
+        },
+        methods: {
+            mockTableData() {
+                let data = [];
+                data.allChecked = false;
+                data.rows = [];
+                for (let i = 0; i < 10; i++) {
+                    data.rows.push({
+                        checked: false,
+                        no: "00" + (i + 1),
+                        account: parseInt(Math.random().toFixed(10) * 10000000000),
+                        city: "深圳",
+                        mobile: "18677888787 ",
+                        version: "2.0.0 ",
+                        regisetrTime: "2018.02.23 12:00:00 ",
+                        num: parseInt(Math.random() * 100)
+                    });
+                }
+                this.tableData = data;
+            },
+            changePage() {
+                // 这里直接更改了模拟的数据，真实使用场景应该从服务端获取数据
+                this.mockTableData();
+            },
+            mockproductModelListData() {
+                let data = [];
+                for (let i = 0; i < 10; i++) {
+                    data.push({
+                        label: "产品型号" + (i + 1),
+                        value: "产品型号" + (i + 1)
+                    });
+                }
+                this.productModelList = data;
+            },
+            mockproductConditionListData() {
+                let data = [];
+                for (let i = 0; i < 10; i++) {
+                    data.push({
+                        label: "产品状态" + (i + 1),
+                        value: "产品状态" + (i + 1)
+                    });
+                }
+                this.productConditionList = data;
+            },
+            check(tableData, item, index) {
+                console.log('1heihouer');
+                if (item.checked) {
+                    item.checked = false;
+                    tableData.allChecked = false;
+                } else {
+                    item.checked = true;
+                    tableData.allChecked = tableData.rows.every(val => {
+                        return !!val.checked;
+                    })
+                }
+            },
+            checkAll(tableData) {
+                console.log('heihouer');
+                tableData.rows.forEach(val => {
+                    val.checked = tableData.allChecked ? false : true;
+                });
+                tableData.allChecked = !tableData.allChecked;
+            }
+        }
+    };
+</script>
+<style lang="less" scoped>
+    .appUser {
+        .fr {
+            float: right;
+        }
+        .content {
+            background: #FFFFFF;
+            box-shadow: 0 2px 4px 0 rgba(31, 49, 89, 0.25);
+            border-radius: 2px;
+            .feedbackSelect {
+                float: right;
+                .selected {
+                    color: #2D8CF0;
+                }
+            }
+            .mytable {
+                width: 100%;
+                th {
+                    padding: 12px 2px;
+                    background: rgba(20, 35, 63, 0.04);
+                    font-size: 12px;
+                    color: #17233D;
+                    text-align: left;
+                    &:first-child,
+                    &:last-child {
+                        text-align: center;
+                    }
+                }
+                td {
+                    padding: 5px 2px;
+                    font-size: 14px;
+                    color: rgba(28, 36, 56, 0.80);
+                    &.feedbackCon {
+                        display: inline-block;
+                        width: 98%;
+                        white-space: nowrap;
+                        overflow: hidden;
+                        text-overflow: ellipsis;
+                        vertical-align: middle;
+                    }
+                    &:first-child,
+                    &:last-child {
+                        text-align: center;
+                    }
+                }
+            }
+            .page {
+                margin-top: 20px;
+                float: right;
+            }
+            h3 {
+                padding: 16px 24px;
+                border-bottom: 1px solid rgba(23, 35, 61, 0.10);
+                font-weight: normal;
+                font-size: 16px;
+                color: #17233D;
+            }
+            h4 {
+                padding: 20px 24px;
+                font-size: 14px;
+                color: rgba(28, 36, 56, 0.80);
+                font-weight: normal;
+            }
+            .posR {
+                position: relative;
+            }
+        }
+    }
+</style>
